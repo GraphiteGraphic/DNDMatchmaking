6f1809d4174ee48cee0aef5f087ffe218e64a385
Ahmad Doleh <ahmad@doleh.com>
2021-04-21 18:21:49 -0400

    Requirements and Preferences in DM submission form visual feedback implemented

--- DND_Matchmaking_Vue/time-keeper/src/components/GameForm.vue
+++ DND_Matchmaking_Vue/time-keeper/src/components/GameForm.vue
@@ -69,11 +69,17 @@
         id="prestige"
       />
     </div>
+
     <div id="requirement">
       Required Attributes:
       <div>
         <label for="req-Faction">Faction: </label>
-        <select name="req-Faction" id="req-Faction" v-model="reqFaction" @change="AddReq(reqFaction)">
+        <select
+          name="req-Faction"
+          id="req-Faction"
+          v-model="reqFaction"
+          @change="AddReq(reqFaction, 'faction')"
+        >
           <option value="null"></option>
           <option v-for="faction in factions" :key="faction" :value="faction">
             {{ faction }}
@@ -83,7 +89,12 @@
 
       <div>
         <label for="req-Race">Race: </label>
-        <select name="req-Race" id="req-Race">
+        <select
+          name="req-Race"
+          id="req-Race"
+          v-model="reqRace"
+          @change="AddReq(reqRace, 'race')"
+        >
           <option value="Null"></option>
           <option v-for="race in races" :key="race" :value="race">
             {{ race }}
@@ -93,12 +104,17 @@
 
       <div>
         <label for="req-Subrace">Subrace: </label>
-        <select name="req-Subrace" id="req-Subrace">
+        <select
+          name="req-Subrace"
+          id="req-Subrace"
+          v-model="reqSubrace"
+          @change="AddReq(reqSubrace, 'subrace')"
+        >
           <option value="Null"></option>
           <option
             v-for="subrace in subraces"
             :key="subrace.race + '_' + subrace.name"
-            :value="subrace.name"
+            :value="subrace.race + ': ' + subrace.name"
           >
             {{ subrace.race }}: {{ subrace.name }}
           </option>
@@ -107,7 +123,12 @@
 
       <div>
         <label for="req-Origin">Origin: </label>
-        <select name="req-Origin" id="req-Origin">
+        <select
+          name="req-Origin"
+          id="req-Origin"
+          v-model="reqOrigin"
+          @change="AddReq(reqOrigin, 'origin')"
+        >
           <option value="Null"></option>
           <option v-for="origin in origins" :key="origin" :value="origin">
             {{ origin }}
@@ -117,7 +138,12 @@
 
       <div>
         <label for="req-Suborigin">Suborigin: </label>
-        <select name="req-Suborigin" id="req-Suborigin">
+        <select
+          name="req-Suborigin"
+          id="req-Suborigin"
+          v-model="reqSuborigin"
+          @change="AddReq(reqSuborigin, 'suborigin')"
+        >
           <option value="Null"></option>
           <option
             v-for="suborigin in suborigins"
@@ -131,25 +157,94 @@
 
       <div>
         <label for="req-Deity">Deity: </label>
-        <select name="req-Deity" id="req-Deity">
+        <select
+          name="req-Deity"
+          id="req-Deity"
+          v-model="reqDeity"
+          @change="AddReq(reqDeity, 'deity')"
+        >
           <option value="Null"></option>
           <option v-for="deity in deities" :key="deity" :value="deity">
             {{ deity }}
           </option>
         </select>
       </div>
-
-      <section name="required-list" id="required-list">
-        <p v-for="req in requirements" :key="req">{{ req }}</p>
-      </section>
     </div>
+
+    <section name="required-list" id="required-list">
+      <div>
+        Faction:
+        <p
+          v-for="req in requirements.faction"
+          :key="req"
+          @click="DeleteReq(req, 'faction')"
+        >
+          {{ req }}
+        </p>
+      </div>
+      <div>
+        Race:
+        <p
+          v-for="req in requirements.race"
+          :key="req"
+          @click="DeleteReq(req, 'race')"
+        >
+          {{ req }}
+        </p>
+      </div>
+      <div>
+        Subrace:
+        <p
+          v-for="req in requirements.subrace"
+          :key="req"
+          @click="DeleteReq(req, 'subrace')"
+        >
+          {{ req }}
+        </p>
+      </div>
+      <div>
+        Origin:
+        <p
+          v-for="req in requirements.origin"
+          :key="req"
+          @click="DeleteReq(req, 'origin')"
+        >
+          {{ req }}
+        </p>
+      </div>
+      <div>
+        Suborigin:
+        <p
+          v-for="req in requirements.suborigin"
+          :key="req"
+          @click="DeleteReq(req, 'suborigin')"
+        >
+          {{ req }}
+        </p>
+      </div>
+      <div>
+        Deity:
+        <p
+          v-for="req in requirements.deity"
+          :key="req"
+          @click="DeleteReq(req, 'deity')"
+        >
+          {{ req }}
+        </p>
+      </div>
+    </section>
 
     <div id="preference">
       Preferred Attributes:
 
       <div>
         <label for="pref-Faction">Faction: </label>
-        <select name="pref-Faction" id="pref-Faction">
+        <select
+          name="pref-Faction"
+          id="pref-Faction"
+          v-model="prefFaction"
+          @change="AddPref(prefFaction, 'faction')"
+        >
           <option value="Null"></option>
           <option v-for="faction in factions" :key="faction" :value="faction">
             {{ faction }}
@@ -159,7 +254,12 @@
 
       <div>
         <label for="pref-Race">Race: </label>
-        <select name="pref-Race" id="pref-Race">
+        <select
+          name="pref-Race"
+          id="pref-Race"
+          v-model="prefRace"
+          @change="AddPref(prefRace, 'race')"
+        >
           <option value="Null"></option>
           <option v-for="race in races" :key="race" :value="race">
             {{ race }}
@@ -169,12 +269,17 @@
 
       <div>
         <label for="pref-Subrace">Subrace: </label>
-        <select name="pref-Subrace" id="pref-Subrace">
+        <select
+          name="pref-Subrace"
+          id="pref-Subrace"
+          v-model="prefSubrace"
+          @change="AddPref(prefSubrace, 'subrace')"
+        >
           <option value="Null"></option>
           <option
             v-for="subrace in subraces"
             :key="subrace.race + '_' + subrace.name"
-            :value="subrace.name"
+            :value="subrace.race + ': ' + subrace.name"
           >
             {{ subrace.race }}: {{ subrace.name }}
           </option>
@@ -183,7 +288,12 @@
 
       <div>
         <label for="pref-Origin">Origin: </label>
-        <select name="pref-Origin" id="pref-Origin">
+        <select
+          name="pref-Origin"
+          id="pref-Origin"
+          v-model="prefOrigin"
+          @change="AddPref(prefOrigin, 'origin')"
+        >
           <option value="Null"></option>
           <option v-for="origin in origins" :key="origin" :value="origin">
             {{ origin }}
@@ -193,7 +303,12 @@
 
       <div>
         <label for="pref-Suborigin">Suborigin: </label>
-        <select name="pref-Suborigin" id="pref-Suborigin">
+        <select
+          name="pref-Suborigin"
+          id="pref-Suborigin"
+          v-model="prefSuborigin"
+          @change="AddPref(prefSuborigin, 'suborigin')"
+        >
           <option value="Null"></option>
           <option
             v-for="suborigin in suborigins"
@@ -207,16 +322,83 @@
 
       <div>
         <label for="pref-Deity">Deity: </label>
-        <select name="pref-Deity" id="pref-Deity">
+        <select
+          name="pref-Deity"
+          id="pref-Deity"
+          v-model="prefDeity"
+          @change="AddPref(prefDeity, 'deity')"
+        >
           <option value="Null"></option>
           <option v-for="deity in deities" :key="deity" :value="deity">
             {{ deity }}
           </option>
         </select>
       </div>
-
-      <section name="pref-list" id="pref-list"></section>
     </div>
+
+    <section name="pref-list" id="pref-list">
+      <div>
+        Faction:
+        <p
+          v-for="pref in preferences.faction"
+          :key="pref"
+          @click="DeletePref(pref, 'faction')"
+        >
+          {{ pref }}
+        </p>
+      </div>
+      <div>
+        Race:
+        <p
+          v-for="pref in preferences.race"
+          :key="pref"
+          @click="DeletePref(pref, 'race')"
+        >
+          {{ pref }}
+        </p>
+      </div>
+      <div>
+        Subrace:
+        <p
+          v-for="pref in preferences.subrace"
+          :key="pref"
+          @click="DeletePref(pref, 'subrace')"
+        >
+          {{ pref }}
+        </p>
+      </div>
+      <div>
+        Origin:
+        <p
+          v-for="pref in preferences.origin"
+          :key="pref"
+          @click="DeletePref(pref, 'origin')"
+        >
+          {{ pref }}
+        </p>
+      </div>
+      <div>
+        Suborigin:
+        <p
+          v-for="pref in preferences.suborigin"
+          :key="pref"
+          @click="DeletePref(pref, 'suborigin')"
+        >
+          {{ pref }}
+        </p>
+      </div>
+      <div>
+        Deity:
+        <p
+          v-for="pref in preferences.deity"
+          :key="pref"
+          @click="DeletePref(pref, 'deity')"
+        >
+          {{ pref }}
+        </p>
+      </div>
+    </section>
+
     <input type="submit" value="Submit" />
   </form>
 </template>
@@ -225,10 +407,35 @@
 export default {
   data() {
     return {
-      reqFaction: '',
-      requirements: [],
-      preferences: []
-    }
+      reqFaction: "",
+      reqRace: "",
+      reqSubrace: "",
+      reqOrigin: "",
+      reqSuborigin: "",
+      reqDeity: "",
+      prefFaction: "",
+      prefRace: "",
+      prefSubrace: "",
+      prefOrigin: "",
+      prefSuborigin: "",
+      prefDeity: "",
+      requirements: {
+        faction: [],
+        race: [],
+        subrace: [],
+        origin: [],
+        suborigin: [],
+        deity: [],
+      },
+      preferences: {
+        faction: [],
+        race: [],
+        subrace: [],
+        origin: [],
+        suborigin: [],
+        deity: [],
+      },
+    };
   },
   computed: {
     factions() {
@@ -248,22 +455,28 @@ export default {
     },
     deities() {
       return this.$store.state.deity;
-    }
+    },
   },
   methods: {
-    AddReq(value) {
-      if (!this.requirements.includes(value) && value != 'null') {
-        this.requirements.push(value);
-        this.requirements = this.requirements.sort();
+    AddReq(value, prop) {
+      if (!this.requirements[prop].includes(value) && value != "null") {
+        this.requirements[prop].push(value);
+        this.requirements[prop] = this.requirements[prop].sort();
       }
     },
-    AddPref(value) {
-      if (!this.preferences.includes(value) && value != 'null') {
-        this.preferences.push(value);
-        this.preferences = this.preferences.sort();
+    DeleteReq(value, prop) {
+      this.requirements[prop].splice(this.requirements[prop].indexOf(value), 1);
+    },
+    AddPref(value, prop) {
+      if (!this.preferences[prop].includes(value) && value != "null") {
+        this.preferences[prop].push(value);
+        this.preferences[prop] = this.preferences[prop].sort();
       }
     },
-  }
+    DeletePref(value, prop) {
+      this.preferences[prop].splice(this.preferences[prop].indexOf(value), 1);
+    },
+  },
 };
 </script>
 
@@ -284,5 +497,27 @@ div > input,
 div > select,
 div > textarea {
   flex-grow: 1;
+}
+#required-list,
+#pref-list {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  text-align: left;
+}
+#required-list > div,
+#pref-list > div {
+  display: flex;
+  flex-direction: row;
+}
+#required-list p,
+#pref-list p {
+  padding-right: 1vw;
+}
+p:hover {
+  text-decoration: line-through;
+  color: grey;
+  cursor: pointer;
 }
 </style>
