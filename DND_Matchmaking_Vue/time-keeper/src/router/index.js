a33506e641435bb631e1310663eda39bce225e21
Ahmad Doleh <ahmad@doleh.com>
2021-04-24 17:08:41 -0400

    discord integration progress

--- DND_Matchmaking_Vue/time-keeper/src/router/index.js
+++ DND_Matchmaking_Vue/time-keeper/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import PlayerSide from '@/views/PlayerSide.vue'
 import DMSide from '@/views/DMSide.vue'
+import Login from '@/views/Login.vue'
 import store from '@/store/index.js'
 
 Vue.use(VueRouter)
@@ -25,6 +26,14 @@ const router = new VueRouter({
       meta: {
         requiresAuth: true
       }
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+      meta: {
+        requiresAuth: false
+      }
     }
   ]
 })
@@ -34,7 +43,7 @@ router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
   // If it does and they are not logged in, send the user to "/login"
   if (requiresAuth && store.state.token === '') {
-    window.location.href = "https://discord.com/oauth2/authorize?client_id=835298512962846750&redirect_uri=htttp%3A%2F%2Flocalhost%3A8080&response_type=token&scope=identify";
+    next("/login")
   } else {
     // Else let them go to their next destination
     next();
---
+++ DND_Matchmaking_Vue/time-keeper/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <a id="login" :href="redirect">Login</a>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      redirect:
+        "https://discord.com/api/oauth2/authorize?client_id=835298512962846750&redirect_uri=http%3A%2F%2Flocalhost%3A8080&response_type=token&scope=identify",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
